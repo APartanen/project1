e9af885356600febb23147534787e28e097ac040
Antti Partanen <a.partanen30@gmail.com>
2021-07-06 16:08:47 +0300

    Axios valmistelu

---
+++ src/components/Content.js
@@ -0,0 +1,36 @@
+import React from "react";
+          
+const Content = (props) => {
+//Axios here
+/*
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+  */
+
+  
+function postContent() {
+
+}
+
+    return(
+        <div>
+
+        
+        </div>
+        
+    );
+};
+
+export default Content;
+
