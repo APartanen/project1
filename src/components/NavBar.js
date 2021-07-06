5b1768e0aa234b57134cea99dfe919b580e4786b
Antti Partanen <a.partanen30@gmail.com>
2021-07-06 19:54:49 +0300

    nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavBar = (props) => {
+
+    return(
+        <div>
+            <nav class="navbar navbar-default bg-dark">
+            
+            </nav>
+        </div>
+    );
+};
+
+export default NavBar;
+
