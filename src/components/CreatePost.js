26ca431f392553661369029b7da0cce14251bf24
Antti Partanen <a.partanen30@gmail.com>
2021-07-07 12:40:07 +0300

    fetch

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -4,13 +4,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const CreatePost = (props) => {
 
+    postForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const formData = new FormData(this); //(this) refers to form - creates form data object
+        fetch('send to', { 
+            method: 'POST',
+            body: formData
+        }).then(function (response){
+            return response.text();
+        }).then(function(text){
+            console.log(text);
+        }).catch(function(error) {
+            console.error(error);
+        })
+    })
+
     return(
         <div>
-            <form method="POST">
+            <form method="POST" id="postForm" class="postForm">
                 <textarea id="textContent" name="textContentArea" rows="8" cols="50">
                     Postaus
                 </textarea> <br/>
-                <input type="submit" value="Submit"/> 
+                <button type="submit" value="Submit">Submit</button>
+               
             </form>
 
        
