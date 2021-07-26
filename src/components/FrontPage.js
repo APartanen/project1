5e99d34254832ca2598f2c75e9534c728b34f469
Antti Partanen <a.partanen30@gmail.com>
2021-07-26 17:16:29 +0300

    changes

---
+++ src/components/FrontPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+//import '../styles/Content.css';
+
+const FrontPage = (props) => {
+
+    return(
+      <div>
+      
+      </div> 
+    );
+};
+
+export default FrontPage;
---
+++ src/components/Posts.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useState } from 'react';
+
+import '../styles/Posts.css';
+
+const Posts = (props) => {
+    const [posts, setPosts] = useState();
+
+    return(
+      <div className="postContainer">
+        
+      </div> 
+    );
+};
+
+export default Posts;
